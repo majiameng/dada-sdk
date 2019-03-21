d3abe80842cfe131dff3290c3f7562378e9f540d
Your Name <you@example.com>
2019-03-21 21:16:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 哒哒
+
+# 哒哒
+
+
+
+### 安装
+
+```
+composer require tinymeng/dada 1.0.0
+```
+
+> 类库使用的命名空间为`\\tinymeng\\oauth`
+
---
+++ src/OAuth.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ *
+ * @author: JiaMeng <666@majiameng.com>
+ */
+namespace tinymeng\Dada;
+
+abstract class Dada
+{
+
+    /**
+     * Description:  init
+     * @author: JiaMeng <666@majiameng.com>
+     * Updater:
+     * @param $gateway
+     * @param null $config
+     * @return mixed
+     * @throws \Exception
+     */
+    protected static function init($gateway, $config = null)
+    {
+        $baseConfig = [
+            'app_id'    => '',
+            'app_secret'=> '',
+            'callback'  => '',
+            'scope'     => '',
+        ];
+        $gateway = Str::uFirst($gateway);
+        $class = __NAMESPACE__ . '\\Gateways\\' . $gateway;
+        if (class_exists($class)) {
+            $app = new $class(array_replace_recursive($baseConfig,$config));
+            if ($app instanceof GatewayInterface) {
+                return $app;
+            }
+            throw new \Exception("第三方登录基类 [$gateway] 必须继承抽象类 [GatewayInterface]");
+        }
+        throw new \Exception("第三方登录基类 [$gateway] 不存在");
+    }
+
+    /**
+     * Description:  __callStatic
+     * @author: JiaMeng <666@majiameng.com>
+     * Updater:
+     * @param string $gateway
+     * @param array $config
+     * @return mixed
+     */
+    public static function __callStatic($gateway, $config)
+    {
+        return self::init($gateway, ...$config);
+    }
+
+}
